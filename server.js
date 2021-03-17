c7cd20a7e383fbdcadd8f402818d10ae18f71908
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-03-17 08:22:26 -0400

    mongoDB intro. signed up on atals and compass. connected the app, compass and atlas. installed mongoose.

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 //server---
-
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-
 dotenv.config({ path: "./config.env" });
 const app = require("./app.js");
+
+const db = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((conn) => {
+    // console.log(conn.connections);
+    console.log("db connection established!");
+  });
 // console.log(app.get("env"));
 console.log(process.env.NODE_ENV);
 
