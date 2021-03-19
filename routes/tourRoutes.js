19b42617ba17edbde71fe6c2136d6b793df2353b
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-03-19 09:42:18 -0400

    added mongoose aggregator pipeline and virtual properties

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -2,7 +2,8 @@ const express = require("express");
 const tourController = require("./../controllers/tourController.js");
 const router = express.Router();
 // router.param("id", tourController.checkID);
-
+router.route("/tour-stats").get(tourController.getTourStats);
+router.route("/monthly-plan/:year").get(tourController.getMonthlyPlan);
 router
   .route("/top-5-cheap")
   .get(tourController.aliasTopTours, tourController.getAllTours);
