6e40767676cffb8f4e367c45d438b3269aa1b506
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-03-29 16:41:32 -0400

    implemented user authorization. for the delete route

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -17,6 +17,7 @@ exports.signUp = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
   const token = signToken(newUser._id);
   res.status(201).json({
@@ -88,3 +89,13 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+exports.restrictTo = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to perform this action", 403)
+      );
+    }
+    next();
+  };
+};
