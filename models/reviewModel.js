baed01f64fa3c8ec77955d5ab75e68df1a24d021
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-04-12 10:16:07 -0400

    implemented average rating calculations on realted tour when any CRUD is performed on Reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const Tour = require("./tourModel.js");
 const reviewSchema = new mongoose.Schema(
   {
     review: {
@@ -42,6 +42,47 @@ reviewSchema.pre(/^find/, function (next) {
   // //   select: "name",
   // // });
   next();
+});
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5,
+    });
+  }
+};
+reviewSchema.post("save", function () {
+  //this points to current review.
+  // Review.calcAverageRatings(this.tour); == cant work coz Review is not yet defined. if we place this under Review declaration the schema wont have the presave middleware. so...
+  //this.constructor also means the Model. so this works..
+  this.constructor.calcAverageRatings(this.tour);
+});
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  //this is the current query-- so we execute a query and get the document on 'this'
+  this.r = await this.findOne();
+
+  next();
+});
+reviewSchema.post(/^findOneAnd/, async function () {
+  //await this.findOne();-- this doesnt work here because the query has already been executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 const Review = new mongoose.model("Review", reviewSchema);
 module.exports = Review;
