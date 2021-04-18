2d0120c4151fba29a7fd6d596769e57a675d0e9d
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-04-18 16:57:19 -0400

    added tours overview page/homepage

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,16 @@
-exports.getOverview = (req, res) => {
+const Tour = require("./../models/tourModel.js");
+const catchAsync = require("./../utils/catchAsync.js");
+exports.getOverview = catchAsync(async (req, res, next) => {
+  //get tour data from collection
+  const tours = await Tour.find();
+  //build template
+
+  //render that template using data.
   res.status(200).render("overview", {
     title: "All tours",
+    tours,
   });
-};
+});
 exports.getTour = (req, res) => {
   res.status(200).render("tour", {
     title: "The Forest Hiker",
