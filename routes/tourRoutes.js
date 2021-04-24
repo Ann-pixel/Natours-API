225fe9b19c2e9198f6a7d18cd278c5581854c7d5
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-04-24 11:00:29 -0400

    implemented image uploads for tours, through postman

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -44,6 +44,8 @@ router
   .patch(
     authenticationController.protect,
     authenticationController.restrictTo("admin", "lead-guide"),
+    tourController.uploadTourImages,
+    tourController.resizeTourImages,
     tourController.updateTour
   )
   .delete(
